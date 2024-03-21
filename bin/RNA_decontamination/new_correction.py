e643c423be722d9f6dea14eb1c128890fea3297f
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2024-03-21 12:29:30 +0100

    update scratch micheals

--- bin/RNA_decontamination/new_correction.py
+++ bin/RNA_decontamination/new_correction.py
@@ -63,13 +63,22 @@ def process_consensus_UMI(df, correction_threshold=3):     # SPOSTARE in helpers
     GBCs = df.index.unique() # Should we get rid of the .unique()?
 
     if GBCs.size == 1:
-        l = [ GBCs[0], 1, 1 ] 
+        l = [ GBCs[0], df['count'].sum(), 1 ] 
     else:
-        numeric_GBCs = np.array([ [ ord(char) for char in s ] for s in GBCs ])
-        D = 18 * pairwise_distances(numeric_GBCs, metric='hamming')
+        numeric_GBCs = np.array([ [ ord(char) for char in s ] for s in GBCs ]) 
+        D = 18 * pairwise_distances(numeric_GBCs, metric='hamming')                     # pdist
         if (D<=correction_threshold).all():
-            top_GBC = GBCs[0]
+            # Simplest alternative: majority barcode as consensus
+            top_GBC = GBCs[0]                                   
             l = [ top_GBC, df['count'].sum(), df.loc[top_GBC, 'count']/df['count'].sum() ]
+            # More advanced (but more computationally intensive...)
+            """
+            AAAC 40
+            ACGC 35
+            ACGT 30
+            ACGA 25
+            top_GBC = ACGC
+            """
         else:
             l =  [np.nan] * 3
 
@@ -89,16 +98,36 @@ def f():
     monitoring convenience.
     """
     processed = (
-        counts
+        counts#.head(100000)
         .groupby(['CBC', 'UMI'])
-        .apply(lambda x: process_consensus_UMI(x))  # Need to be speeded up in parallel, if possible...
+        .apply(lambda x: process_consensus_UMI(x)) # Need to be speeded up in parallel, if possible...
+        # pandasparallel --> soluzione pandas-specifica
+        # simpler and more general data structure + joblib parallelization --> soluzione general purpose
         .droplevel(2).reset_index()
-        .dropna()                                   # Filter out CBC-UMIs with too heterogeneous GBC sets
+        .dropna()                                  # Filter out CBC-UMIs with too heterogeneous GBC sets
     )
     return processed
 
 # Run code and monitor time
-processed = run_command(f, verbose=True)            # ~4 minutes.
+processed = run_command(f, verbose=True)           # ~4 minutes.
+
+
+##
+
+
+
+# UMI-deduplication
+UMIs = processed.query('CBC=="AAACCCAAGCCTCCAG"')['UMI'].values.tolist()
+numeric_UMIs = np.array([ [ ord(char) for char in s ] for s in UMIs ]) 
+D = 12 * pairwise_distances(numeric_UMIs, metric='hamming') 
+np.sum((D==1).sum(axis=1)==1) / D.shape[0]
+
+# rows, cols = np.where(D==1)
+# umi1 = UMIs[rows[0]]
+# umi2 = UMIs[cols[0]]
+# processed.query('CBC=="AAACCCAAGCCTCCAG" and UMI==@umi1')['GBC'].values[0] == \
+# processed.query('CBC=="AAACCCAAGCCTCCAG" and UMI==@umi2')['GBC'].values[0]
+
 
 
 ##
@@ -134,22 +163,51 @@ M, df_combos = filter_and_pivot(
     normalized_abundance_treshold=normalized_abundance_treshold
 )
 
+
+
+
+"""
+Extend process_consensus_UMI con metodo piu' fancy.
+Quantificare UMI-duplication partedo dagli UMI Weinreb et al., o dai Micheals et al. --> Distribuzione (per cellula) della frazione di UMI<=1H distance.
+N cellule e numero di UMI (CBC-UMI-GBC combinations) che rimangono (filtering UMI Weinreb et al., o dai Micheals et al.)
+Sul campione a 8 GBC dimmi quanti GBC rimangono dopo Micheals. 
+
+Date:
+    umi_treshold=umi_treshold, 
+    p_treshold=p_treshold, 
+    max_ratio_treshold=max_ratio_treshold, 
+    normalized_abundance_treshold=normalized_abundance_treshold
+in tre i casi (filtering UMI Weinreb et al. con treshold sul minimo, o dai Micheals et al.)
+Quantifica MOI dalla M nei due casi, e il numero di cellule che si porterebbero a casa prendendo solo le singole.
+"""
+
+
+
+
+
+
+
+
+
+
+##
+
+
 # Visualize CBC-GBC filtering
-fig, ax = plt.subplots(figsize=(5,5))
-sns.kdeplot(data=df_combos, x='normalized_abundance', y='max_ratio', ax=ax)
-fig.tight_layout()
-plt.show()
+# fig, ax = plt.subplots(figsize=(5,5))
+# sns.kdeplot(data=df_combos, x='normalized_abundance', y='max_ratio', ax=ax)
+# fig.tight_layout()
+# plt.show()
 
 # Cell assignment, final
-unique_cells = (M>0).sum(axis=1).loc[lambda x: x==1].index
-filtered_M = M.loc[unique_cells]
-clones_df = get_clones(filtered_M)
-cells_df = (
-    filtered_M
-    .apply(lambda x: filtered_M.columns[x>0][0], axis=1)
-    .to_frame('GBC')
-)
-
+# unique_cells = (M>0).sum(axis=1).loc[lambda x: x==1].index
+# filtered_M = M.loc[unique_cells]
+# clones_df = get_clones(filtered_M)
+# cells_df = (
+#     filtered_M
+#     .apply(lambda x: filtered_M.columns[x>0][0], axis=1)
+#     .to_frame('GBC')
+# )
 
 # Write out and save
 # ...
