3563462a4862910a3b0ca8171dedc7bfea5d61fc
Guido Campani <ieo6943@MBP-di-Campani.ieo.it>
2024-04-15 10:08:42 +0200

    get_grepped_file now is generale for all type of experiment and sample

--- bin/RNA_decontamination/get_grepped_file.py
+++ bin/RNA_decontamination/get_grepped_file.py
@@ -2,22 +2,66 @@
 
 import dnaio
 import os
+import argparse
 
-# File path
-sample = 'a'
-type = 'b'
-name = 'c'
 
-#main_path = f'/hpcnfs/scratch/PGP/MI_TO_benchmark/preprocessing/pp/data/maester/AML_clones'
-main_path = f'/hpcnfs/scratch/PGP/MI_TO_benchmark/preprocessing/pp/data/sc_tenx/AML_clones'
-input_read_1 = os.path.join(main_path, "S45555_854_S11_L001_R1_001.fastq.gz")
-input_read_2 = os.path.join(main_path,"S45555_854_S11_L001_R2_001.fastq.gz")
-#output_read = "/hpcnfs/home/ieo6943/scracth/grepped.txt"
+##
 
-#main_path = f'/hpcnfs/scratch/PGP/MI_TO_benchmark/preprocessing/pp/data/{type}/{sample}'
-#input_read_1 = os.path.join(main_path, f"{name}_R1_001.fastq.gz")
-#input_read_2 = os.path.join(main_path,f"{name}_R2_001.fastq.gz")
-output_read = "/hpcnfs/home/ieo6943/scracth/grepped_sc_tenex.txt"
+
+# Create the parser
+my_parser = argparse.ArgumentParser(
+    prog='get_grepped_file',
+    description=
+    """
+    Get grepped file.
+    """
+)
+
+my_parser.add_argument(
+    '--type', 
+    type=str,
+    default=None,
+    help='type of experiment: lentiviral, GEX, mitocondrial'
+)
+
+my_parser.add_argument(
+    '--sample', 
+    type=str,
+    default=None,
+    help='Datasample: AML_clones, MDA_clones, ...'
+)
+
+my_parser.add_argument(
+    '--read1', 
+    type=str,
+    default=None,
+    help='Read1'
+)
+
+my_parser.add_argument(
+    '--read2', 
+    type=str,
+    default=None,
+    help='read2'
+)
+
+
+# Parse arguments
+args = my_parser.parse_args()
+type =args.type
+sample = args.sample
+read1 = args.read1
+read2 = args.read2
+
+
+##
+
+
+#Arg
+main_path = f'/hpcnfs/scratch/PGP/MI_TO_benchmark/preprocessing/pp/data/{type}/{sample}'
+input_read_1 = os.path.join(main_path, read1)
+input_read_2 = os.path.join(main_path,read2)
+output_read = f"/hpcnfs/home/ieo6943/results/{type}/grepped.txt"
     
 #
         
