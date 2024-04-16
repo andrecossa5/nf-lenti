28f891bb1b5ba6d911251f4d2d3b4a0772628739
Guido Campani <ieo6943@MacBook-Pro-di-Campani.local>
2024-04-16 20:43:57 +0200

    print out of part of the read2

--- bin/RNA_decontamination/get_grepped_file.py
+++ bin/RNA_decontamination/get_grepped_file.py
@@ -3,6 +3,7 @@
 import dnaio
 import os
 import argparse
+import gzip
 
 
 ##
@@ -53,7 +54,6 @@ sample = args.sample
 read1 = args.read1
 read2 = args.read2
 
-
 ##
 
 
@@ -62,10 +62,32 @@ main_path = f'/hpcnfs/scratch/PGP/MI_TO_benchmark/preprocessing/pp/data/{type}/{
 input_read_1 = os.path.join(main_path, read1)
 input_read_2 = os.path.join(main_path,read2)
 output_read = f"/hpcnfs/home/ieo6943/results/{type}/{sample}/grepped.txt"
-    
-#
 
-print('input_read=', input_read_1)
+print('path_read1=', input_read_1 )
+print('path_read2=', input_read_2)
+
+print('read2')
+#main_path = f'/Users/ieo6943/Documents/data/AML_clones/'
+#input_read_1 = os.path.join(main_path, 'head_S45545_854_enrichment_PCR_S2_L001_R1_001.fastq.gz')
+#input_read_2 = os.path.join(main_path,'head_S45545_854_enrichment_PCR_S2_L001_R2_001.fastq.gz')
+#output_read = os.path.join(main_path,'enrich_grepped.txt')
+#    
+##
+#
+#print('input_read=', input_read_1)
+#print(output_read)
+#
+with gzip.open(input_read_2, 'rb') as file:
+    # Leggi le prime 8 righe
+    for _ in range(8):
+        riga = file.readline()
+        print(riga.decode().strip())
+#
+#with open(output_read, 'r') as file:
+#    # Leggi le prime dieci righe
+#    for i in range(10):
+#        riga = file.readline()
+#        print(riga.strip())
         
 with dnaio.open(input_read_1, input_read_2) as reader, open(output_read,'w') as writer:
     writer.write(f"Name\tcbc\tumi\tfeature\n")
