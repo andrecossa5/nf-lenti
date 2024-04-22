c5355b4680a4c318271a4d45bdc1012f35efc595
Guido Campani <ieo6943@MacBook-Pro-di-Campani.local>
2024-04-22 18:10:40 +0200

    modification to read all types of data

--- bin/RNA_decontamination/get_grepped_file.py
+++ bin/RNA_decontamination/get_grepped_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
 import dnaio
-import os
 import argparse
 import pandas as pd
+from scipy.spatial.distance import hamming
 
 
 ##
@@ -11,36 +11,36 @@ import pandas as pd
 
 # Create the parser
 my_parser = argparse.ArgumentParser(
-    prog='get_grepped_file',
+    prog='get_triplets',
     description=
     """
-    Get grepped file.
+    Get grepped file. 
     """
 )
 
 my_parser.add_argument(
-    '--type', 
+    '--input_path', 
     type=str,
     default=None,
-    help='type of experiment: lentiviral, GEX, mitocondrial'
+    help='tinput path of the data'
 )
 
 my_parser.add_argument(
-    '--sample', 
+    '--output_path', 
     type=str,
     default=None,
-    help='Datasample: AML_clones, MDA_clones, ...'
+    help='Where to save the files'
 )
 
 my_parser.add_argument(
-    '--read1', 
+    '--read1_path', 
     type=str,
     default=None,
     help='Read1'
 )
 
 my_parser.add_argument(
-    '--read2', 
+    '--read2_path', 
     type=str,
     default=None,
     help='read2'
@@ -53,38 +53,42 @@ my_parser.add_argument(
     help='path of the barcodes'
 )
 
+my_parser.add_argument(
+    '--gbc',
+    action='store_true',
+    help='filtering for lentiviral. Default: False.'
+)
+
+my_parser.add_argument(
+    '--anchor',
+    type=str,
+    default=None,
+    help='anchor for lentiviralbarcoding'
+)
+
+my_parser.add_argument(
+    '--threshold',
+    type=int,
+    default=1,
+    help='hamming distance from the anchor'
+)
 
 # Parse arguments
 args = my_parser.parse_args()
-type =args.type
-sample = args.sample
-read1 = args.read1
-read2 = args.read2
+input_path =args.input_path
+input_read_1 = args.read1_path
+input_read_2 = args.read2_path
+barcodes_path = args.barcodes_path
+output_read = args.output_path
+
+
 
 ##
 
-
-#Arg
-main_path = f'/hpcnfs/scratch/PGP/MI_TO_benchmark/preprocessing/pp/data/{type}/{sample}'
-input_read_1 = os.path.join(main_path, read1)
-input_read_2 = os.path.join(main_path,read2)
-path_barcodes = args.barcodes_path
-path_barcodes = os.path.join(path_barcodes, 'barcodes.tsv.gz')
-output_read = f"/hpcnfs/home/ieo6943/results/{type}/{sample}/grepped.txt"
-
-
-#main_path = '/Users/ieo6943/Documents/data/AML_clones/'
-#input_read_1 = os.path.join(main_path, 'head_S45545_854_enrichment_PCR_S2_L001_R1_001.fastq.gz')
-#input_read_2 = os.path.join(main_path,'head_S45545_854_enrichment_PCR_S2_L001_R2_001.fastq.gz')
-#path_barcodes = os.path.join(main_path, 'barcodes.tsv.gz')
-#output_read = os.path.join(main_path,'grepped_scratch.txt.gz')
-
-
-##
 
 
 solo_CBCs = pd.read_csv(
-    path_barcodes, 
+    barcodes_path, 
     header=None, index_col=0
 )
 solo_CBCs = list(solo_CBCs.index.unique())
@@ -95,5 +99,13 @@ with dnaio.open(input_read_1, input_read_2) as reader, open(output_read,'w') as 
         cbc = record_1.sequence[:16]
         umi = record_1.sequence[16:]
         feature = record_2.sequence
-        if  cbc in solo_CBCs:
-            writer.write(f"{record_1.name}\t{cbc}\t{umi}\t{feature}\n")
+        if args.gbc==True:
+                    a_ = feature[:33]
+                    gbc = feature[33:33+18]
+                    h = hamming(list(a_), list(args.anchor)) * 33
+                    if h <= int(args.threshold) and cbc in solo_CBCs:
+                        feature = gbc
+                        writer.write(f"{record_1.name}\t{cbc}\t{umi}\t{feature}\n")
+        else:
+            if cbc in solo_CBCs:
+                 writer.write(f"{record_1.name}\t{cbc}\t{umi}\t{feature}\n")
