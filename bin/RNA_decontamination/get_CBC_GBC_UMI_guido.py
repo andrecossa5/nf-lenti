8b72121afa47d4697f85a9e69b5c2af937a69e5a
Guido Campani <ieo6943@MBP-di-Campani.ieo.it>
2024-04-22 12:39:42 +0200

    modified

--- bin/RNA_decontamination/get_CBC_GBC_UMI_guido.py
+++ bin/RNA_decontamination/get_CBC_GBC_UMI_guido.py
@@ -50,9 +50,10 @@ args = my_parser.parse_args()
 type =args.type
 sample = args.sample
 grepped_path = args.grepped_path
-path_barcodes = args.barcodes_path
-path_barcodes = os.path.join(path_barcodes, 'barcodes.tsv.gz')
+#path_barcodes = args.barcodes_path
+#path_barcodes = os.path.join(path_barcodes, 'barcodes.tsv.gz')
 path_data = os.path.join(grepped_path, 'grepped.txt.gz')
+
 
 ##
 
@@ -60,10 +61,9 @@ path_data = os.path.join(grepped_path, 'grepped.txt.gz')
 #type = 'sc'
 #sample = 'AML_clones'
 #grepped_path = '/Users/ieo6943/Documents/data/AML_clones/'
-#path_barcodes = '/Users/ieo6943/Documents/data/AML_clones/'
-#path_barcodes = os.path.join(path_barcodes, 'barcodes.tsv.gz')
-#path_data = os.path.join(grepped_path, 'grepped.txt.gz')
-
+#path_barcodes = os.path.join(grepped_path, 'barcodes.tsv.gz')
+#path_data = '/Users/ieo6943/Documents/data/AML_clones/grepped_scratch.txt.gz'
+#path_file_GBC = '/Users/ieo6943/Documents/data/AML_clones/GBC_read_elements.tsv.gz'
 
 ##
 
@@ -76,18 +76,17 @@ treshold = 1
 
 
 # Read Solo-filtered CBCs 
-solo_CBCs = pd.read_csv(
-    path_barcodes, 
-    header=None, index_col=0
-)
+#solo_CBCs = pd.read_csv(
+#    path_barcodes, 
+#    header=None, index_col=0
+#)
 
 # From unsorted bam, grepped
 file_in = gzip.open(path_data, 'rb')
 next(file_in)
-file_out = gzip.open(f'/hpcnfs/home/ieo6943/results/{type}/{sample}/GBC_read_elements.tsv.gz', 'wb')
-#file_out = gzip.open('/Users/ieo6943/Documents/data/AML_clones/GBC_read_elements.tsv.gz', 'wb')
-anchor = 'TAGCAAACTGGGGCACAAGCTTAATTAAGAATT'
-i=0
+file_out = gzip.open(f'/hpcnfs/home/ieo6943/results/{type}/{sample}/GBC_read_elements_scratch.tsv.gz', 'wb')
+#file_out = gzip.open(path_file_GBC, 'wb')
+#i=0
 for line in file_in:
     fields = line.decode('utf-8').strip().split('\t')
     #print(f'fields:{fields}')
@@ -110,15 +109,16 @@ for line in file_in:
             print('++++++++++++++++++++++++++++++++++++++++++++')
         else:
             print('----------------------------------')
-        if h <= int(treshold) and cr in solo_CBCs.index:
-            i+=1
+        if h <= int(treshold):
+            #i+=1
             r = f'@{read_name}\t{cr}\t{ur}\t{gbc}\n'
             file_out.write(r.encode('utf-8'))
-            print(i)
-            if i==10000:
-                break
+            #print(i)
+            #if i==10000:
+                #break
 
 
 
 file_in.close()
 file_out.close()
+
