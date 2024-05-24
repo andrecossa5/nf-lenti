753f1febcbfcdee2cf79d9f22ab81257febb4597
Guido Campani <ieo6943@MacBook-Pro-di-Campani.local>
2024-05-24 14:59:42 +0200

    consensus_bam fith filter in python

---
+++ bin/sc_gbc/filter_consensus.py
@@ -0,0 +1,123 @@
+import pysam
+import numpy as np
+
+
+import argparse
+
+# Create the parser
+my_parser = argparse.ArgumentParser(
+    prog='cell_assignment',
+    description=
+    """
+    Script for clone calling and cell assignment.
+    """
+)
+
+my_parser.add_argument(
+    '--input', 
+    type=str,
+    default=None,
+    help='Path input bam. should come from fgbio CallMolecularConsensus'
+)
+
+my_parser.add_argument(
+    '--output', 
+    type=str,
+    default=None,
+    help='Path output bam'
+)
+
+my_parser.add_argument(
+    '--base_quality_th', 
+    type=int,
+    default=None,
+    help='Quality threshold of each base expressed with Phred score, bases with lower quality are masked with N '
+)
+
+my_parser.add_argument(
+    '--mean_quality_th', 
+    type=int,
+    default=None,
+    help='Mean quality of the whole reads expressed with Phred score, reads with lower mean quality are filtered out '
+)
+
+my_parser.add_argument(
+    '--E', 
+    type=float,
+    default=None,
+    help='The maximum raw-read error rate across the entire consensus read. Reads with higher erreor are filtered out '
+)
+
+my_parser.add_argument(
+    '--e', 
+    type=float,
+    default=None,
+    help='The maximum error rate for a single consensus base. bases with higer error are masked with N'
+)
+
+my_parser.add_argument(
+    '--mask_th', 
+    type=float,
+    default=None,
+    help='The maximum density of N in a sequence after masking.'
+)
+
+##
+
+# Parse arguments
+args = my_parser.parse_args()
+bam_file = args.input
+output_bam = args.output
+quality_th = args.base_quality_th
+mean_qual_th = args.mean_quality_th
+max_Er = args.E
+base_er = args.e
+mask_th = args.mask_th
+
+def filter_consensus(bam_file, output_bam, quality_th, mean_qual_th,max_Er,base_er,mask_th):
+
+    bam_in = pysam.AlignmentFile(bam_file, "rb", check_sq=False)
+    bam_out = pysam.AlignmentFile(output_bam, "wb", template=bam_in)
+    #j=0
+    for read in bam_in.fetch(until_eof=True):
+        n_consensus_read = read.get_tag('cM:')
+        mean_consensus_error = read.get_tag('cE:')
+        base_consensus_error = np.array(read.get_tag('ce:'))/n_consensus_read
+        high_consensus_err_index = np.where(base_consensus_error>base_er)[0]
+        #filter mean error
+        if mean_consensus_error>max_Er:
+            continue
+        sequence = list(read.query_sequence)
+        qualities = np.array(read.query_qualities)
+        mean_qualities = qualities.sum()/qualities.shape[0]
+        # filter mean quality
+        if mean_qualities<mean_qual_th: 
+            continue
+        
+        #MASKs for the bad bases
+        low_quality_indices = np.where(qualities < quality_th)[0]
+        # Replace low quality bases with 'N'
+        for i in low_quality_indices:
+            sequence[i] = 'N'
+        for i in high_consensus_err_index:
+            sequence[i] = 'N'
+        bad_reads_density=sequence.count('N')/len(sequence)
+        #filter if the mask is too high
+        if bad_reads_density>mask_th:
+            continue
+        read.query_sequence = ''.join(sequence)
+        bam_out.write(read)
+        #j+=1
+        #if j==10: break
+    bam_in.close()
+    bam_out.close()
+
+def main():
+        filter_consensus(bam_file, output_bam, quality_th, mean_qual_th,max_Er,base_er,mask_th)
+
+
+
+
+if __name__ == "__main__":
+    main()
+
--- subworkflows/sc_gbc/modules/consensus_bam.nf
+++ subworkflows/sc_gbc/modules/consensus_bam.nf
@@ -16,20 +16,58 @@ process CONSENSUS_BAM {
 
   script:
   """
+
   # Group reads by UMI
   fgbio="java -Xmx 4000m -jar + /maegatk/maegatk /bin/fgbio.jar" 
   fgbio GroupReadsByUmi \
       -i ${cell_folder}/${cell}/${cell}.bam \
       -o grouped.bam \
+
+  fgbio="java -Xmx4000m -jar /maegatk/maegatk/bin/fgbio.jar"
+
+  $fgbio GroupReadsByUmi \
+      -i ${cell_folder}/${cell}.bam \ 
+      -o ${cell_folder}/grouped.bam \
       -s ${params.fgbio_UMI_consensus_mode} \
       -e ${params.fgbio_UMI_consensus_edits} \
       -t UB \
       -T MI    
+
+  # 2.5) Modify the UB tag
+  #samtools view -H ${cell_folder}/grouped.bam > ${cell_folder}/grouped_temp.bam 
+  #samtools view ${cell_folder}/grouped.bam | awk \'OFS="\t" {$13=$13""$4; print $0}\' >> ${cell_folder}/grouped_temp.sam
+  #samtools view -b ${cell_folder}/grouped_temp.sam > ${cell_folder}/grouped_temp.bam 
+  #os.system('echo "'+samtoolscall+'"')
+  #os.system(samtoolscall)
+
+  # 2.5) Modify the UB tag
+  #samtools view -H TGTAACGCATTCTCTA/grouped.bam > TGTAACGCATTCTCTA/grouped_temp.bam 
+  #samtools view TGTAACGCATTCTCTA/grouped.bam | awk \'OFS="\t" {$13=$13""$4; print $0}\' >> TGTAACGCATTCTCTA/grouped_temp.sam
+  #samtools view -b TGTAACGCATTCTCTA/grouped_temp.sam > TGTAACGCATTCTCTA/grouped_temp.bam 
   
   # Call Molecular consensus reads
-  fgbio CallMolecularConsensusReads -t UB -i grouped.bam -o consensus.bam -M ${params.fgbio_min_reads} 
+  # -i ${cell_folder}/grouped_temp.bam
+  # -o {cell_folder}/consensus.bam
+  $fgbio CallMolecularConsensusReads -t UB -i ${cell_folder}/grouped.bam -o ${cell_folder}/consensus.bam -M ${params.fgbio_min_reads} 
+  
+
+  # $fgbio CallMolecularConsensusReads -t UB -i TGTAACGCATTCTCTA/grouped.bam -o TGTAACGCATTCTCTA/consensus.bam -M 3
+
 
   # Filter high-quality consensus reads
+  python ${baseDir}/bin/sc_gbc/filter_consensus.py \
+  --input consensus.bam \
+  --output filtered.bam \
+  --base_quality 30 \
+  --mean_quality_th 30 \
+  --E 0.025 \
+  --e 0.1 \
+  --mask_th 0.2
+  #$fgbio FilterConsensusReads -i TGTAACGCATTCTCTA/consensus.bam  -o TGTAACGCATTCTCTA/TGTAACGCATTCTCTA_consensus_filtered.bam -r $ref -M 3 -e 0.1 -N 20 -E 0.025
+
+
+
+  #attenzione a params ref
   # fgbio FilterConsensusReads \
   #     -i consensus.bam \
   #     -o ${cell}_consensus_filtered.bam \
@@ -40,7 +78,7 @@ process CONSENSUS_BAM {
   #     -E 0.025   
 
   # Index consensus_bam
-  samtools index ${cell}_consensus.bam
+  #samtools index ${cell}_consensus.bam
   """
 
   stub:
