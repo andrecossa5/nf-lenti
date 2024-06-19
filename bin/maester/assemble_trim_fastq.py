ac6ea10d21d7a24ffb05a89718cab23c6c281a37
Guido Campani <ieo6943@MacBook-Pro-di-Campani.local>
2024-06-19 10:57:13 +0200

    correctio in assembled.fastq.gz of assemble trim.py

--- bin/maester/assemble_trim_fastq.py
+++ bin/maester/assemble_trim_fastq.py
@@ -13,7 +13,7 @@ def main():
 
     cpus = math.floor(float(cpus))
     with dnaio.open(R1, R2, mode='r', open_threads=cpus) as reader, \
-        dnaio.open(f'assembled.fastq.gz', mode='w', open_threads=cpus) as writer:
+        dnaio.open('assembled.fastq.gz', mode='w', open_threads=cpus) as writer:
         for r1, r2 in reader: 
             cbc, umi = r1.sequence[:16], r1.sequence[16:] 
             r2.name = '_'.join( r2.name.replace('/', '.').split(' ') + [cbc] + [umi] )
