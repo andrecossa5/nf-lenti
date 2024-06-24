df55907464ad9e1ec41162cfde2431f75e7442ed
Guido Campani <ieo6943@MBP-di-Campani.ieo.it>
2024-06-24 18:03:46 +0200

    correction in gathering allelic tables

--- bin/maester/process_files.sh
+++ bin/maester/process_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 files=("$@")
 for f in "${files[@]}"; do  
+    echo $f
     if [[ "$f" =~ .T\\.txt$ ]]; then  
         cat "$f" >> T_cells.txt
     elif [[ "$f" =~ .G\\.txt$ ]]; then  
