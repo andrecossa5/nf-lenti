75d4f6355223b6c6bbadff3b6cbc8a18938e871f
Andrea Cossa <IEO5505@Cossas-MacBook-Pro.local>
2024-10-04 15:10:20 +0200

    format freebayes call: retain only SNVs for direct benchmarking

--- bin/benchmark/collapse_bulk_methods.py
+++ bin/benchmark/collapse_bulk_methods.py
@@ -20,6 +20,19 @@ def read_samtools_call(path):
 ##
 
 
+def read_freebayes_call(path):
+    cell = path.split('_')[0]
+    df = pd.read_csv(path, sep='\t', header=None)
+    df.columns = ['CHROM', 'POS', 'ID', 'REF', 'ALT', 'AD', 'DP']
+    df = df.loc[df['REF'].map(lambda x: len(x)==1)]                  # Only SNVs here
+    df['AD'] = df['AD'].map(lambda x: int(x.split(',')[1]))
+    df = df.assign(cell=cell)
+    return df  
+
+
+##
+
+
 # Args 
 method = sys.argv[1] 
 
@@ -31,7 +44,8 @@ def main():
 
     if method == 'samtools':
         calls = [ read_samtools_call(x) for x in os.listdir() if x.endswith('filtered.tsv') ]
-    
+    elif method == 'freebayes':
+        calls = [ read_freebayes_call(x) for x in os.listdir() if x.endswith('filtered.tsv') ]
 
     calls = pd.concat(calls)
     calls.to_csv(f'{method}_allele_table.csv.gz')
